794eab0344e07a4dd1637f1a8fa6210a8f6f4f97
William Mora <wilmor24@gmail.com>
2015-01-04 14:35:00 -0300

    direction tests

---
+++ game/src/test/java/com/nispok/cityconnection/game/DirectionTest.java
@@ -0,0 +1,20 @@
+package com.nispok.cityconnection.game;
+
+import com.nispok.cityconnection.game.enums.Direction;
+import junit.framework.TestCase;
+
+public class DirectionTest extends TestCase {
+
+    public void testThereShouldBeTwoDirections() {
+        assertEquals(2, Direction.values().length);
+    }
+
+    public void testValueOfLeftShouldReturnLeft() {
+        assertEquals(Direction.LEFT, Direction.valueOf("LEFT"));
+    }
+
+    public void testValueOfRightShouldReturnRight() {
+        assertEquals(Direction.RIGHT, Direction.valueOf("RIGHT"));
+    }
+
+}
